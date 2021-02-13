0982996c2964c7c777ace7a1293cf6bb0fda2863
0xfinlay <finlay.richardson1@gmail.com>
2021-02-13 17:17:27 -0500

    Update help

--- cogs/useful.py
+++ cogs/useful.py
@@ -12,11 +12,12 @@ class Useful(commands.Cog):
         helpMessage = discord.Embed(
             title="Help", colour=discord.Colour.red())
         for cog in cogs:
-            commands = self.client.get_cog(cog).get_commands()
-            text = "\n".join(
-                [f"`{c.name}`: {c.brief} {c.description}" for c in commands]
-            )
-            helpMessage.add_field(name=f"{cog}", value=text, inline=False)
+            if cog != "Admin":
+                commands = self.client.get_cog(cog).get_commands()
+                text = "\n".join(
+                    [f"`{c.name}`: {c.brief} {c.description}" for c in commands]
+                )
+                helpMessage.add_field(name=f"{cog}", value=text, inline=False)
 
         await ctx.send(embed=helpMessage)
 
